dbcb42e20b8c4cf0c34c4c08cdcad10fff06da98
Karan Rathore <karansingh19feb92@gmail.com>
2017-03-16 12:06:27 +0530

    Initial commit

---
+++ app/src/main/java/in/co/b2bhub/www/fragmentdemo/RegisterFragment.java
@@ -0,0 +1,20 @@
+package in.co.b2bhub.www.fragmentdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by B2B Android on 1/25/2017.
+ */
+
+public class RegisterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.reges_fragment_layout,container,false);
+    }
+}
